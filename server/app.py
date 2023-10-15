14860570b76cadc6b9a4ff10ed8b13475b818b1e
Watheq Attrah <novaatrah@gmail.com>
2023-10-14 23:54:39 -0700

    Server Base Complete

--- server/app.py
+++ server/app.py
@@ -1,23 +1,154 @@
 #!/usr/bin/env python3
-
-# Standard library imports
-
-# Remote library imports
-from flask import request
+from flask import jsonify, make_response, render_template, session, request
+from sqlalchemy.ext.associationproxy import association_proxy
 from flask_restful import Resource
+from sqlalchemy import func
+from sqlalchemy.exc import IntegrityError
+from config import app, db, api, bcrypt
+from models import User, Review, Car
 
-# Local imports
-from config import app, db, api
-# Add your model imports
 
+# ============#============#============#============#============
+# API's Starts Here
 
-# Views go here!
 
 @app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+@app.route('/<int:id>')
+def index(id=0):
+    return render_template("index.html")
+
+# ClearSession
+
+
+class ClearSession(Resource):
+    def delete(self):
+        session['page_views'] = None
+        session['user_id'] = None
+        return {}, 204
+
+
+api.add_resource(ClearSession, '/clear', endpoint='clear')
+
+# ============#============#============#============#============
+# Check_Session
+
+
+class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id') == None:
+            return {}, 204
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        return user.to_dict(), 200
+
+
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
+# ============#============#============#============#============
+
+
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+            # Successful authentication
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        # Authentication failed
+        return {'error': '401 Unauthorized'}, 401
+
+
+api.add_resource(Login, '/login', endpoint='login')
+
+# ============#============#============#============#============
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+
+api.add_resource(Logout, '/logout', endpoint='logout')
+# ============#============#============#============#============
+
+
+class Signup(Resource):
+    def post(self):
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        if username and password:
+
+            new_user = User(username=username)
+            new_user.password_hash = password
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+
+            return new_user.to_dict(), 201
+
+        return {'error': '422 Unprocessable Entity'}, 422
+
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+
+# ============#============#============#============#============
+
+
+class Cars(Resource):
+    def get(self):
+        cars = []
+        for car in Car.query.all():
+            car = {
+                "id": car.id,
+                "make": car.make,
+                "model": car.model,
+                "year": car.year,
+                "image": car.image,
+                "price": car.price,
+                "vin": car.vin,
+                "engine": car.engine,
+                "miles": car.miles,
+            }
+            cars.append(car)
+        return make_response(jsonify(cars), 200)
+
+
+api.add_resource(Cars, '/cars', endpoint='cars')
+# ============#============#============#============#============
+
+
+class CarByID(Resource):
+    # get reviews for a specific car
+    def get(self, id):
+        reviews = Review.query.filter_by(car_id=id)
+        reviews_data = {'reviews': [
+            {'id': review.id, 'review': review.review, 'user': review.user.username} for review in reviews]}
+        return make_response(jsonify(reviews_data), 200)
+
+    # add a review for a specific car
+    def post(self, id):
+        data = request.get_json()
+        user_id = data['user_id']
+        car_id = data['car_id']
+        review_text = data['review_text']
+
+        car = Car.query.get(id)
+        if car:
+            review = Review(review=review_text, car_id=car_id, user_id=user_id)
+            db.session.add(review)
+            db.session.commit()
+        return {'message': 'Review added successfully'}, 201
+
+
+api.add_resource(CarByID, '/cars/<int:id>')
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
--- server/models.py
+++ server/models.py
@@ -1,6 +1,87 @@
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt
+from sqlalchemy.orm import validates
 
-from config import db
+# ===============#===============#===============#===============#===============#===============#===============
 
-# Models go here!
+
+class Car(db.Model, SerializerMixin):
+    __tablename__ = 'cars'
+
+    serialize_rules = ('-reviews.car',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(20))
+    model = db.Column(db.String(20))
+    year = db.Column(db.Integer())
+    image = db.Column(db.String(50))
+    price = db.Column(db.Float(precision=2))
+    vin = db.Column(db.String(17), unique=True)
+    engine = db.Column(db.Integer)
+    miles = db.Column(db.Float)
+
+    reviews = db.relationship('Review', backref='car')
+
+    def __repr__(self):
+        return f'<Car Id: {self.id}, Make: {self.make}, Model: {self.model}, Year: {self.year},Price: {self.price},VIN: {self.vin},Engine: {self.engine},Image: {self.image}>'
+
+# ===============#===============#===============#===============#===============#===============#===============
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    serialize_rules = ('-car.reviews', '-user.reviews',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    car_id = db.Column(db.Integer, db.ForeignKey('cars.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Review Id: {self.id},  {self.review}>'
+
+# ===============#===============#===============#===============#===============#===============#===============
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-reviews.user',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), unique=True, nullable=False)
+    email = db.Column(db.String)
+    admin = db.Column(db.String, default=False)
+
+    _password_hash = db.Column(db.String)
+
+    reviews = db.relationship('Review', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    # email validatiom
+    @validates('email')
+    def validate_email(self, key, address):
+        if '@' not in address:
+            raise ValueError('failed simple email validation')
+        return address
+
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'
